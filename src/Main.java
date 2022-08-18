6633127b96f3e00be17227627a0b0714d97b0538
Vitalis-prog147 <vitalis.programming28@gmail.com>
2022-08-18 23:05:59 +0300

    re

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Objects;
 
-
 public class Main {
-    public static void main(String[] args) throws ScanerException {
+    public static void main(String[] args) throws ScannerException {
         Scanner in = new Scanner(System.in);
         System.out.println(
                 "Доброго дня.\n" +
@@ -12,163 +10,102 @@ public class Main {
                         "Программа предназначена для арифметических действий (сложение, вычитание, деление, умножения) только с двумя аргументами одинакового вида.\n" +
                         "Операнды и операторы необходимо отделить пробелами."
         );
-        System.out.print("Ожидаю ввода выражения: ");
-        String calculationExpression = in.nextLine(); // Ввод выражения вычесления
-        String[] expression = calculationExpression.split(" ");
+        System.out.print(
+                "Ожидаю ввода выражения: ");
+        String calculationExpressionInput = in.nextLine(); // Ввод выражения вычисления (Entering a calculation expression)
+        System.out.println(calc(calculationExpressionInput));
 
-        int lengthExpression = expression.length; // определение длины массива
-        //System.out.println (lengthExpression);
-        int lehgthArray = 3;
+        in.close();
+    }
+    public static String calc (String input) throws ScannerException {
+        //Метод должен принимать строку с арифметическим выражением между двумя числами и возвращать строку с результатом их выполнения
+        String[] expression = input.split(" "); // Разбитие строки на массивы (Splitting a string into arrays)
+        int lengthExpression = expression.length; // Определение длины массива (Determining the length of an array)
+        int lengthArray = 3; // Переменная отвечающая за логику отсеивания, условие : выражение состоит из двух переменных и одного операнда
+        // (The variable responsible for the filtering logic, condition : the expression consists of two variables and one operand)
 
-        if (lengthExpression < lehgthArray) {
-            throw new ScanerException("throws Exception //т.к. строка не является математической операцией");
-        }
-        if (lengthExpression > lehgthArray) {
-            throw new ScanerException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        }
-        String[] referenceNumbers = new String[12]; // создания массива арабских чисел для сравнения
-
-        for (int i = 0; i < referenceNumbers.length; i++) { //заполнение массива числами для сравнения
-            referenceNumbers[i] = String.valueOf(i);
-        }
-        RomanNumerals[] Rome = RomanNumerals.values(); // создания массива с данными Enam, для сравнения
-        //System.out.println(Arrays.toString(Rome));
-
-
-
-
-        boolean arg1 = false; // для проверки типа цифр
-        boolean arg2 = false;
-
-        for (int i=0; i < referenceNumbers.length; i++){ //проверка принадежности первого операнда к арабским числам
-
-            if (Objects.equals(expression [0], referenceNumbers [i])){
-                //System.out.println("Первый операнд арабское число");
-                arg1 = true;
-                break;
-            }
-        }
-        for (int i=0; i < referenceNumbers.length; i++){//проверка принадежности второго операнда к арабским числам
-
-            if (Objects.equals(expression [2], referenceNumbers [i])){
-                arg2 = true;
-                break;
-            }
-            else {
-
-            }
+        if (lengthExpression < lengthArray) {
+            throw new ScannerException(
+                    "throws Exception //т.к. строка не является математической операцией");
         }
 
+        if (lengthExpression > lengthArray) {
+            throw new ScannerException("" +
+                    "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
 
-            if (arg1 == arg2 && arg1 == true) { // вычисление арабских чисел
+        if (checkingRomanNumbers(expression[0]) && checkingRomanNumbers(expression[2])) {
+            input = RomanNumerals.arabicRecordConstructor(expression[0])+" "+expression[1]+" "+RomanNumerals.arabicRecordConstructor(expression[2]);
+            return RomanNumerals.RomanNumerals(calculationBlock(input));
+        }
+        else if (checkingArabicNumbers(expression[0]) && checkingArabicNumbers(expression[2])){
+            return String.valueOf(calculationBlock(input));
+        }
 
-                for (int j=0; j < referenceNumbers.length; j++){ //проверка операндов на < 11
-                    if (Integer.valueOf(expression[0]) > 10 || Integer.valueOf(expression[2]) > 10){
-                        throw new ScanerException("throws Exception //т.к. введеные числа арументов больше 10");
-                    }
-                }
+        else { // Блок вычислений №3, прочие числа или одновременные операции с римскими и арабскими цифрами
+            // (Calculation block No. 3, other numbers or simultaneous operations with Roman and Arabic numerals)
+            throw new ScannerException(
+                    "throws Exception //т.к. \n" +
+                            "Вариант № 1: используются одновременно разные системы счисления.\n" +
+                            "Вариант № 2: в выражении один из операндов или оба не является арабскими или римскими числами.\n" +
+                            "Вариант № 3: в выражении используются дробные числа.");
+        }
 
-                //System.out.println("Операнды являются арабскими числами");
-
-                switch (expression[1]) {
-                    case "+":
-                        System.out.print ("Результат вычисления: ");
-                        System.out.println (Integer.valueOf(expression[0]) + Integer.valueOf(expression[2]));
-                        break;
-                    case "-":
-                        System.out.print("Результат вычисления: ");
-                        System.out.println (Integer.valueOf(expression[0]) - Integer.valueOf(expression[2]));
-                        break;
-                    case "*":
-                        System.out.print("Результат вычисления: ");
-                        System.out.println (Integer.valueOf(expression[0]) * Integer.valueOf(expression[2]));
-                        break;
-                    case "/":
-                        System.out.print("Результат вычисления: ");
-                        double a = Double.valueOf(expression[0]) / Double.valueOf(expression[2]);
-                        System.out.println (Integer.valueOf((int) a));
-                        break;
-                    default:
-                        throw new ScanerException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - использованы операторы помимо (+, -, /, *)");
-                }
-            }
-            else if (arg1 == arg2 && arg1 == false) { // вычисление римских чисел
-
-                for (int i=0; i < referenceNumbers.length; i++){ //проверка операндов на < XI
-                    if (Objects.equals(expression[0], Rome[i].name())) {
-                        int a = Integer.parseInt(Rome[i].getTranslation());
-                        if (a > 10){
-                            throw new ScanerException("throws Exception //т.к. введеные числа арументов больше X");
-                        }
-                    }
-                    if (Objects.equals(expression[2], Rome[i].name())) {
-                        int a = Integer.parseInt(Rome[i].getTranslation());
-                        if (a > 10){
-                            throw new ScanerException("throws Exception //т.к. введеные числа арументов больше X");
-                        }
-                    }
-                }
-
-                //System.out.println("Операнды являются римскими числами");
-
-                for (int i = 0; i < Rome.length; i++) { // Сравнение римских чисел и конвертация в арабские для Первого операнда
-                    if (Objects.equals(expression[0], Rome[i].name())) {
-                        expression[0] = Rome[i].getTranslation();
-                        break;
-                    }
-                }
-                for (int j = 0; j < Rome.length; j++) { // Сравнение римских чисел и конвертация в арабские для Второго операнда
-                    if (Objects.equals(expression[2], Rome[j].name())) {
-                        expression[2] = Rome[j].getTranslation();
-                        break;
-                    }
-                }
-                int result;
-                switch (expression[1]) {
-                    case "+":
-                        System.out.print("Результат вычисления: ");
-                        result = Integer.valueOf(expression[0]) + Integer.valueOf(expression[2]);
-                        //System.out.println (result);
-                        break;
-                    case "-":
-                        System.out.print("Результат вычисления: ");
-                        result = Integer.valueOf(expression[0]) - Integer.valueOf(expression[2]);
-                        //System.out.println (result);
-                        break;
-                    case "*":
-                        System.out.print("Результат вычисления: ");
-                        result = Integer.valueOf(expression[0]) * Integer.valueOf(expression[2]);
-                        //System.out.println (result);
-                        break;
-                    case "/":
-                        System.out.print("Результат вычисления: ");
-                        result = Integer.valueOf(expression[0]) / Integer.valueOf(expression[2]);
-                        //System.out.println (Integer.valueOf((int) result));
-                        break;
-                    default:
-                        throw new ScanerException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - использованы операторы помимо (+, -, /, *)");
-                }
-                if (result <1){
-                    throw new ScanerException("throws Exception //т.к. в римской системе нет отрицательных чисел, результат вычисления меньше 1");
-                }
-                //System.out.println(Integer.valueOf((int) result));
-
-                for (int i=0; i < Rome.length; i++) { // Сравнение арабских чисел и конвертация результата расчета в римские
-                    int n = Integer.parseInt(Rome[i].getTranslation());
-                    if (Objects.equals(result, n)) {
-                        System.out.println(Rome[i]);
-                        break;
-                    }
-                }
-            }
-                else {
-                throw new ScanerException("throws Exception //т.к. используются одновременно разные системы счисления");
-            }
-
-        // Калькулятор умеет работать только с целыми числами.
-        // Необходима нормальная библиотека римских чисел.
-            in.close();
+    }
+    public static Integer calculationBlock (String input) throws ScannerException {
+        String[] expression = input.split(" ");
+        System.out.print("Результат вычисления: ");
+        switch (expression[1]) {
+            case "+":
+                return Integer.valueOf(expression[0]) + Integer.valueOf(expression[2]);
+            case "-":
+                return Integer.valueOf(expression[0]) - Integer.valueOf(expression[2]);
+            case "*":
+                return Integer.valueOf(expression[0]) * Integer.valueOf(expression[2]);
+            case "/":
+                double a = Double.valueOf(expression[0]) / Double.valueOf(expression[2]);
+                return Integer.valueOf((int) a);
+            default:
+                throw new ScannerException(
+                        "throws Exception //т.к. формат математической операции не удовлетворяет заданию - " +
+                                "использованы операторы помимо (+, -, /, *)");
         }
     }
+    public static Boolean checkingArabicNumbers (String input) throws ScannerException {
+
+        if (Integer.valueOf (input) > 10){//Проверка операндов на < 11 (Checking operands for < 11)
+            throw new ScannerException(
+                    "throws Exception //т.к. введеные числа арументов больше 10");
+        }
+
+        String[] referenceNumbers = new String[12]; // Создания массива арабских чисел для сравнения (Creating an array of Arabic numbers for comparison)
+
+        for (int i = 0; i < referenceNumbers.length; i++) { // Заполнение массива числами для сравнения (Filling an array with numbers for comparison)
+            referenceNumbers[i] = String.valueOf(i);
+        }
+
+        for (int i = 0; i < referenceNumbers.length; i++) { // Проверка принадлежности операнда к арабским числам
+            // (Checking whether the operand belongs to Arabic numbers)
+            if (Objects.equals(input, referenceNumbers[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static Boolean checkingRomanNumbers (String input) throws ScannerException {
+
+        for (int i = 1; i < 4000; i++) { // Проверка принадлежности операнда к римским числам
+            // (Checking whether the operand belongs to Roman numbers)
+            if (Objects.equals(input, RomanNumerals.RomanNumerals(i))) {
+                if (RomanNumerals.arabicRecordConstructor(RomanNumerals.RomanNumerals(i))> 10) {//проверка операндов на < XI (Checking operands on < XI)
+                    throw new ScannerException(
+                            "throws Exception //т.к. введеные числа арументов больше X");
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
 
--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,28 +1,55 @@
-public enum RomanNumerals {
-    I(1),
-    II(2),
-    III(3),
-    IV(4),
-    V(5),
-    VI(6),
-    VII(7),
-    VIII(8),
-    IX(9),
-    X(10),
-    XI(11),
-    XII(12);
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
 
-    public static int length;
-    private String translation;
-    RomanNumerals (Integer translation){
-        this.translation = String.valueOf(translation);
+public class RomanNumerals {
+
+    public static String RomanNumerals(int arabicNumber) throws ScannerException {
+
+        if (arabicNumber < 1 || arabicNumber > 3999) {
+            throw new ScannerException("В римской системе исчесления нет чисел меньше 1 или больше 3999" +
+                    "/In the Roman system of calculus, there are no numbers less than 1 or greater than 3999");
+        }
+        LinkedHashMap<String, Integer> roman_numerals = new LinkedHashMap<String, Integer>();
+        roman_numerals.put("M", 1000);
+        roman_numerals.put("CM", 900);
+        roman_numerals.put("D", 500);
+        roman_numerals.put("CD", 400);
+        roman_numerals.put("C", 100);
+        roman_numerals.put("XC", 90);
+        roman_numerals.put("L", 50);
+        roman_numerals.put("XL", 40);
+        roman_numerals.put("X", 10);
+        roman_numerals.put("IX", 9);
+        roman_numerals.put("V", 5);
+        roman_numerals.put("IV", 4);
+        roman_numerals.put("I", 1);
+        String res = "";
+
+        for (Map.Entry<String, Integer> convertibleNumber : roman_numerals.entrySet()) {
+            int matches = arabicNumber / convertibleNumber.getValue();
+            res += romeRecordConstructor(convertibleNumber.getKey(), matches);
+            arabicNumber = arabicNumber % convertibleNumber.getValue();
+        }
+
+        return res;
     }
-
-    public String getTranslation() {
-        return translation;
+    public static String romeRecordConstructor(String s, int n) {
+        if (s == null) {
+            return null;
+        }
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            stringBuilder.append(s);
+        }
+        return stringBuilder.toString();
     }
-
-
-
-
+    public static Integer arabicRecordConstructor(String s) throws ScannerException {
+        for (int i = 1; i < 4000; i++) {
+            if (Objects.equals(s, RomanNumerals.RomanNumerals(i))) {
+                return i;
+            }
+        }
+        return null;
+    }
 }
